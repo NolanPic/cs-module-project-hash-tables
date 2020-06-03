701acd44c62d0368d7ff27dc2f6b4cff071b0de4
Nolan Picini <nolanpic@gmail.com>
2020-06-03 13:29:45 -0700

    complete no_dupes

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,19 @@
-def no_dups(s):
-    # Your code here
+# loop thru split string
+#     if word not in cache
+#         add to new string
+#     add word to cache
 
+def no_dups(s):
+    cache = {}
+    # split string by space into array of words
+    words = s.split()
+    new_s = ''
+    for word in words:
+        if word not in cache:
+            # build the new string with no dupes
+            new_s += word + ' '
+            cache[word] = True
+    return new_s.rstrip() # <- trim the last trailing space off the end
 
 
 if __name__ == "__main__":
