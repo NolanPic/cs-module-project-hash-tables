f7566ced95983cfde63f1edca8d8f078619d2db5
Nolan Picini <nolanpic@gmail.com>
2020-06-03 19:34:34 -0700

    remove unnecessary code

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -2,10 +2,6 @@ import re
 
 with open("robin.txt") as f:
     words = f.read()
-    
-# keep track of the longest word so we can
-# format properly when printing to console
-longest_word_length = 0
 
 # lookup table for number-to-hashmarks
 hashmarks = {}
@@ -17,16 +13,12 @@ def gen_hashmark_lookup(length):
         hashmarks[i] = hashes
 
 def get_sorted_word_counts(s):
-    global longest_word_length
     
     counts = {}
     
     # split the string by spaces to get all words
     words = s.split()
     for word in words:
-        # update longest word count
-        if len(word) > longest_word_length:
-            longest_word_length = len(word)
         
         # clean and lowercase the word
         word = clean(word.lower())
@@ -43,12 +35,10 @@ def clean(word):
     return re.sub('[^a-zA-Z\']', '', word)
 
 def print_histo(sorted_word_counts):
-    global longest_word_length
     
     for i in sorted_word_counts.items():
         word = i[0]
         count = i[1]
-        longest_word_length = longest_word_length + 4
         print(f'{word:20}{hashmarks[count]}')
     
 
