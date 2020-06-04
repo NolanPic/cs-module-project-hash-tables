783be8c443f888b7c1b90e03d07c95734db9b319
Nolan Picini <nolanpic@gmail.com>
2020-06-03 18:11:04 -0700

    complete lookup_table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,17 @@
-# Your code here
+import math
+import random
 
+lookup = {}
+def build_lookup():
+    
+    # need all possible combinations of x and y
+    for x in range(2, 14):
+        for y in range(3, 6):
+            v = math.pow(x, y)
+            v = math.factorial(v)
+            v //= (x + y)
+            v %= 982451653
+            lookup[(x, y)] = v
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -14,10 +26,10 @@ def slowfun(x, y):
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
-    # Your code here
+    return lookup[(x, y)]
 
 
-
+build_lookup()
 # Do not modify below this line!
 
 for i in range(50000):
